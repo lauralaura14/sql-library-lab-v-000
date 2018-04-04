1976967f2aacba74f01bde8625851c2311ca50a4
Laura <laurao148@gmail.com>
2018-04-04 03:04:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.subgenre_id
    FROM authors
    INNER JOIN series
-   ON authors.id = series.authors_id
+   ON authors.id = series.author_id
    GROUP BY authors.name"
 end
 
