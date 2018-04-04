9c8cc276dad92648e7b7fac8e35860c9aede2652
Laura <laurao148@gmail.com>
2018-04-04 03:01:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,9 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.subgenres
-  "
+   FROM authors
+   INNER JOIN series
+   ON authors.id = "
 end
 
 def select_series_title_with_most_human_characters
