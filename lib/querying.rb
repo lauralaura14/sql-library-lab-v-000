c36ea692fede28690aeb27caf04a5acecadfa8f9
Laura <laurao148@gmail.com>
2018-04-04 03:07:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,10 +21,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name
+  "SELECT authors.name, series.subgenre_id
    FROM authors
    INNER JOIN series
    ON authors.id = series.author_id
+   JOIN subgenres
+   ON series.subgenre_id = subgenre.id
    GROUP BY authors.name"
 end
 
