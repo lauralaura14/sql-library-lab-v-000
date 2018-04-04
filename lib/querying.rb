bbf8adc3fe429816639857ba72489c107fc8856d
Laura <laurao148@gmail.com>
2018-04-04 02:50:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,8 +15,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT *
-   FROM characters"
+  "SELECT species, COUNT(species) AS species
+   FROM characters
+   ORDER BY species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
